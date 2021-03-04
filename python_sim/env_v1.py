e3ad11adfd6d13ccac03bf7ada570d79ed4f01bc
JiatongSun <SJT_sjtu1998@163.com>
2021-03-04 15:29:38 -0500

    add image preprocessing

--- python_sim/env_v1.py
+++ python_sim/env_v1.py
@@ -1,6 +1,9 @@
 import gym
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+
+from utils import rgb2gray
 
 
 class OccupancyGridEnv(gym.Env):
@@ -13,14 +16,16 @@ class OccupancyGridEnv(gym.Env):
         self.fig = None
 
         if lattice_img_path is not None:
-            #get lattice image from file
-            lattice_img = None
+            # get lattice image from file
+            lattice_img = mpimg.imread(lattice_img_path)
             self.occupancy = self.create_occupancy_from_img(lattice_img)
         else:
             raise NotImplementedError('Must provide lattice image to initialize environment')
 
         self.init_agents()
 
+    def init_agents(self):
+        pass
 
     def reset(self):
         self.init_agents()
@@ -30,18 +35,29 @@ class OccupancyGridEnv(gym.Env):
         self.update_state(action)
 
     def render(self, mode='human'):
-        if mode== 'human':
+        if mode == 'human':
             plt.ion()
 
         if self.fig is None:
-            #initialize figure
+            # initialize figure
             pass
 
     def get_obs(self):
         return self.x
 
-
     def update_state(self, action):
-        self.x = self.x+action
+        self.x = self.x + action
 
-    def create_occupancy_from_img(self,img):
+    def create_occupancy_from_img(self, img):
+        occupancy_map = rgb2gray(img)
+        occupancy_map = np.where(occupancy_map > 0.5, 1, 0)
+        return occupancy_map
+
+
+if __name__ == "__main__":
+    lattice_img_path = "images/candidate_1.png"
+    env = OccupancyGridEnv(lattice_img_path=lattice_img_path, n_agents=3)
+    # print(env.occupancy)
+    # plt.imshow(env.occupancy, cmap=plt.get_cmap('gray'), vmin=0, vmax=1)
+    # plt.show()
+
