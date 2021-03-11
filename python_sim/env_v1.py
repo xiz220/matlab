f9c9b239466cceaad5f478542eb12311187f24ba
wgosrich <wgosrich@gmail.com>
2021-03-11 13:47:32 -0500

    robot init partially functioning

--- python_sim/env_v1.py
+++ python_sim/env_v1.py
@@ -17,15 +17,24 @@ class OccupancyGridEnv(gym.Env):
 
         if lattice_img_path is not None:
             # get lattice image from file
-            lattice_img = mpimg.imread(lattice_img_path)
-            self.occupancy = self.create_occupancy_from_img(lattice_img)
+            self.lattice_img = mpimg.imread(lattice_img_path)
+            self.occupancy = self.create_occupancy_from_img(self.lattice_img)
         else:
             raise NotImplementedError('Must provide lattice image to initialize environment')
 
-        self.init_agents()
+        self.x = self.init_agents()
 
     def init_agents(self):
-        pass
+        num_agents = 0
+        x = []
+        while num_agents < self.n_agents:
+            candidate_x = np.random.randint(0,self.occupancy.shape[1])
+            candidate_y = np.random.randint(0,self.occupancy.shape[0])
+            if self.occupancy[-candidate_y,-candidate_x] == 0:
+                x.append([candidate_x,candidate_y])
+                num_agents = num_agents + 1
+
+        return np.array(x)
 
     def reset(self):
         self.init_agents()
@@ -38,9 +47,50 @@ class OccupancyGridEnv(gym.Env):
         if mode == 'human':
             plt.ion()
 
+        # Content data limits in data units.
+        data_xlim = [0, self.occupancy.shape[1]]
+        data_ylim = [0, self.occupancy.shape[0]]
+        data_aspect_ratio = (data_xlim[1] - data_xlim[0]) / (data_ylim[1] - data_ylim[0])
+
         if self.fig is None:
             # initialize figure
-            pass
+            # Aesthetic parameters.
+            # Figure aspect ratio.
+            fig_aspect_ratio = 16.0 / 9.0  # Aspect ratio of video.
+            fig_pixel_height = 1080  # Height of video in pixels.
+            dpi = 300  # Pixels per inch (affects fonts and apparent size of inch-scale objects).
+
+            # Set the figure to obtain aspect ratio and pixel size.
+            fig_w = fig_pixel_height / dpi * fig_aspect_ratio  # inches
+            fig_h = fig_pixel_height / dpi  # inches
+            self.fig, self.ax = plt.subplots(1, 1,
+                                             figsize=(fig_w, fig_h),
+                                             constrained_layout=True,
+                                             dpi=dpi)
+            self.ax.set_xlabel('x')
+            self.ax.set_ylabel('y')
+
+            # Set axes limits which display the workspace nicely.
+            self.ax.set_xlim(data_xlim[0], data_xlim[1])
+            self.ax.set_ylim(data_ylim[0], data_ylim[1])
+
+            # Setting axis equal should be redundant given figure size and limits,
+            # but gives a somewhat better interactive resizing behavior.
+            self.ax.set_aspect('equal')
+
+            # Draw robots
+            self.robot_handle = self.ax.scatter(self.x[:, 0], self.x[:, 1], 20, 'red')
+
+        self.robot_handle.set_offsets(self.x)
+
+
+        self.fig.canvas.draw()
+        self.fig.canvas.flush_events()
+
+
+        # Add occupancy grid
+        self.ax.imshow(self.occupancy, cmap='gray', vmin=0, vmax=1)
+        plt.show(block=True)
 
     def get_obs(self):
         return self.x
