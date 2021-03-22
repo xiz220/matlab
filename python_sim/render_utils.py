8e721f4183f9a4fe5b1d7765982a1a4af44c3d0f
wgosrich <wgosrich@gmail.com>
2021-03-22 12:39:13 -0400

    updating render_model script to take rule class instance instead

--- python_sim/render_utils.py
+++ python_sim/render_utils.py
@@ -16,13 +16,14 @@ from gym.wrappers.monitoring import video_recorder
 from stable_baselines.common.vec_env import VecVideoRecorder
 
 
-def render_model(model, env, video_folder, name_prefix, n_episodes, action_arrows=False):
+def render_model(rule, env, video_folder, name_prefix, n_episodes, action_arrows=False):
     """
     Save a single video of the model in the environment over n episodes. If a
     video of the same name already exists in the target folder, append a unique
     suffix instead of overwriting.
     Inputs:
-        model,        previously trained model
+        rule,        class with a get_action() member function that gives the next action. TODO: this should maybe
+                        be compatible with a tf model as well
         env,          vectorized environment, typically of size 1
         video_folder, directory for output
         name_prefix,  file name with no extension
@@ -38,7 +39,8 @@ def render_model(model, env, video_folder, name_prefix, n_episodes, action_arrow
         count = 0
         obs = video_env.reset()
         while count < n_episodes:
-            action, _states = model.get_action(obs[0]) # can replace with model.predict(obs) if using tf model
+            #get
+            action = rule.get_action(obs[0]) # can replace with model.predict(obs) if using tf model
             obs, rewards, dones = video_env.step(action)
             if dones[0]:
                 count = count + 1
