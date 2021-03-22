d30164f5cb13a0c3c79067925095365872a60564
wgosrich <wgosrich@gmail.com>
2021-03-22 12:38:33 -0400

    adding a scipt that records a video with the fillet rule and the environment. doesn't work yet

---
+++ python_sim/record_video.py
@@ -0,0 +1,16 @@
+from envs.env_v1 import OccupancyGridEnv
+from render_utils import render_model
+from fillet_rule import FilletRule
+from stable_baselines.common.vec_env import DummyVecEnv
+import gym
+import numpy as np
+
+env = OccupancyGridEnv(lattice_img_path='images/candidate_3_clean.png', sensor_model="update_sensor_reading_occupancy")
+rule = FilletRule()
+
+env = DummyVecEnv([gym.make('OccupancyGrid-v0',lattice_img_path='images/candidate_3_clean.png', sensor_model="update_sensor_reading_occupancy")])
+
+render_model(rule, env,
+            video_folder='videos',
+            name_prefix='video',
+            n_episodes=1)
