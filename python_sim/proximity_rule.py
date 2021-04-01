577d8977b87622b9fb02bfe001ce72688acc6fb8
wgosrich <wgosrich@gmail.com>
2021-04-01 13:29:36 -0400

    updating docs for proximity rule

--- python_sim/proximity_rule.py
+++ python_sim/proximity_rule.py
@@ -7,7 +7,7 @@ class ProximityRule:
 
     def get_action(self, obs):
         """
-        Takes in an observation and returns the next action to take to do a wall-following sort of filleting action
+        Takes in an observation and returns the next action: move randomly, deposit if adjacent to existing material.
         :param obs: observation dict {'x': robot_states (n_agents x 2 np array), 'sensor_readings': (n_agents x row_dim x col_dim) np array}
         :return: n_agents x 3 np array, where row i represents the i'th robot's: [x_action, y_action, deposition_action]
         """
--- python_sim/test.py
+++ python_sim/test.py
@@ -6,7 +6,7 @@ from fillet_rule import FilletRule
 n_agents = 10
 render_interval = 1
 env = OccupancyGridEnv(lattice_img_path='images/candidate_3_clean.png', sensor_model="update_sensor_reading_occupancy", n_agents=n_agents)
-rule = ProximityRule()
+rule = FilletRule()
 
 action = np.concatenate(((np.random.rand(n_agents, 2) - 0.5) * 4, np.zeros((n_agents, 1))), axis=1)
 
